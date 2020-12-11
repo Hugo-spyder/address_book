681698bb093ae588bd24ba4f9fd1a7e48a675c08
Hugo-spyder <53307317+Hugo-spyder@users.noreply.github.com>
2020-12-11 20:21:13 +0800

    Update people.cpp

--- people.cpp
+++ people.cpp
@@ -9,13 +9,9 @@ using std::cin;
 using std::cout;
 using std::ifstream;
 using std::ofstream;
-// people methods
-string People::getName() {
-    return name;
-}
 
 // friend people
-ostream& operator<<(ostream &os, const People &peo) {
+ostream &operator<<(ostream &os, const People &peo) {
     os << "Name: " << peo.name << std::endl;
     os << "Telephone: " << peo.telephone << std::endl;
     os << "Address: " << peo.address << std::endl;
@@ -25,7 +21,7 @@ ostream& operator<<(ostream &os, const People &peo) {
     return os;
 }
 
-ofstream &operator<<(ofstream & of, const People & peo){
+ofstream &operator<<(ofstream &of, const People &peo) {
     of << peo.name << std::endl;
     of << peo.telephone << std::endl;
     of << peo.address << std::endl;
@@ -35,47 +31,49 @@ ofstream &operator<<(ofstream & of, const People & peo){
     return of;
 }
 
-istream & operator>>(istream & is, People & peo)
-{
+istream &operator>>(istream &is, People &peo) {
     cout << "Name:";
-    getline(is,peo.name);
+    getline(is, peo.name);
     cout << "Telephone:";
-    getline(is,peo.telephone);
+    getline(is, peo.telephone);
     cout << "Address:";
-    getline(is,peo.address);
+    getline(is, peo.address);
     cout << "Mail:";
-    getline(is,peo.mail);
+    getline(is, peo.mail);
     cout << "Unit:";
-    getline(is,peo.unit);
+    getline(is, peo.unit);
     cout << "Comment:";
-    getline(is,peo.comment);
+    getline(is, peo.comment);
     return is;
 }
 // -------------------------------------------------------------------------------------
 
- // peopleBook methods
- PeopleBook::PeopleBook() {
+// peopleBook methods
+PeopleBook::PeopleBook() {
     head = tail = nullptr;
     peoples = 0;
 }
+
 PeopleBook::~PeopleBook() {
-    Node * temp;
-    while(head != nullptr)
-    {
+    Node *temp;
+    while (head != nullptr) {
         temp = head;
         head = head->next;
         delete temp;
     }
 }
+
 bool PeopleBook::isempty() const {
     return peoples == 0;
 }
+
 int PeopleBook::peopleCount() const {
     return peoples;
 }
+
 // add people to people book
 bool PeopleBook::addpeople(const People &pl) {
-    Node * add = new Node;
+    Node *add = new Node;
     add->people = pl; // 用到了people 类的默认赋值运算符 people类没用new 分配内存
     add->next = nullptr;
     peoples++;
@@ -86,50 +84,44 @@ bool PeopleBook::addpeople(const People &pl) {
     tail = add;        // have tail point to new point
     return true;
 }
+
 // 查找联系人
-PeopleBook::Node * PeopleBook::findpeople(const string &na) {
-   if (isempty())
-       std::cout << "address book is empty, please add people.\n";
-   else
-   {
-       Node * temp = head;
-       while ((temp->people.getName() != na) && (temp->next != nullptr))
-           temp = temp->next;
-       if (temp->people.getName() == na)
-       {
-           std::cout << "The people named " << na << " have been find.\n";
-           return temp;
-       }
-       else
-           std::cout << "This address book don't contain a people who called " << na << std::endl;
-   }
+PeopleBook::Node *PeopleBook::findpeople(const string &na)
+{
+    if (isempty())
+        std::cout << "address book is empty, please add people.\n";
+    else {
+        Node *temp = head;
+        while ((temp->people.getName() != na) && (temp->next != nullptr))
+            temp = temp->next;
+        if (temp->people.getName() == na) {
+            std::cout << "The people named " << na << " have been find.\n";
+            return temp;
+        } else
+            std::cout << "This address book don't contain a people who called " << na << std::endl;
+    }
     return nullptr;
 }
+
 // 依据人名进行删除
-bool PeopleBook::delpeople(const string & na) {
-    if (isempty())
-    {
+bool PeopleBook::delpeople(const string &na) {
+    if (isempty()) {
         std::cout << "\nAddress book is empty.\n";
         return false;
-    }
-    else
-    {
-        Node * temp1 = head;
-        Node * temp2 = head;
-        while((temp1->people.getName() != na) && (temp1->next != nullptr))
-        {
+    } else {
+        Node *temp1 = head;
+        Node *temp2 = head;
+        while ((temp1->people.getName() != na) && (temp1->next != nullptr)) {
             temp2 = temp1;
             temp1 = temp1->next;
         }
-        if (temp1->people.getName() == na)
-        {
+        if (temp1->people.getName() == na) {
             temp2->next = temp1->next;
             std::cout << "Delete " << temp1->people.getName() << " successfully.\n";
             delete temp1;
             peoples--;
             return true;
-        }
-        else
+        } else
             std::cout << "Don't have a contact named " << na << std::endl;
         return false;
     }
@@ -138,8 +130,7 @@ bool PeopleBook::delpeople(const string & na) {
 //  从文件读取数据
 void PeopleBook::loadData() {
     ifstream readFile("addressData.txt");
-    if (!readFile.is_open())
-    {
+    if (!readFile.is_open()) {
         cout << "Can't open the file\n";
         cout << "Program terminating.\n";
         exit(EXIT_FAILURE);
@@ -147,17 +138,25 @@ void PeopleBook::loadData() {
     People people;
     int lines = 0;           // 记录所读取的行数
     string temp;
-    while (readFile.good())
-    {
-        getline(readFile,temp);
+    while (readFile.good()) {
+        getline(readFile, temp);
         lines++;
-        switch (lines % 6)
-        {
-            case 1: people.setName(temp); break;
-            case 2: people.setTele(temp); break;
-            case 3: people.setAddr(temp); break;
-            case 4: people.setMail(temp); break;
-            case 5: people.setUnit(temp); break;
+        switch (lines % 6) {
+            case 1:
+                people.setName(temp);
+                break;
+            case 2:
+                people.setTele(temp);
+                break;
+            case 3:
+                people.setAddr(temp);
+                break;
+            case 4:
+                people.setMail(temp);
+                break;
+            case 5:
+                people.setUnit(temp);
+                break;
             case 0:
                 people.setComm(temp);
                 addpeople(people);
@@ -169,9 +168,8 @@ void PeopleBook::loadData() {
 
 void PeopleBook::outData() {
     ofstream writeFile("addressData.txt");
-    Node * temp = head;
-    while(temp != nullptr)
-    {
+    Node *temp = head;
+    while (temp != nullptr) {
         writeFile << temp->people;
         temp = temp->next;
     }
@@ -179,10 +177,9 @@ void PeopleBook::outData() {
 }
 
 void PeopleBook::showPeopleBk(int nums) {
-    Node * temp = head;
+    Node *temp = head;
     int order = nums;
-    while (nums > 0 && nums < peoples + 1)
-    {
+    while (nums > 0 && nums < peoples + 1) {
         cout << "# " << order - nums + 1 << std::endl;
         cout << "---------------------------------\n";
         cout << temp->people;
@@ -194,4 +191,50 @@ void PeopleBook::showPeopleBk(int nums) {
         cout << "Please enter a integer at least one.\n";
     if (order > peoples)
         cout << "Please enter a integer less than the people total: " << peoples << std::endl;
+}
+
+template<typename T>
+void PeopleBook::swap(T &a, T &b) {
+    T temp = a;
+    a = b;
+    b = temp;
+}
+
+PeopleBook::Node * PeopleBook::partition(PeopleBook::Node *pBegin, PeopleBook::Node *pEnd) {
+    auto pivot = pBegin->people.getName();
+    Node * p = pBegin;
+    Node * q = p->next;
+    while (q != pEnd->next)
+    {
+        if (q->people.getName() < pivot)
+        {
+            p = p->next;
+            swap(p->people,q->people);
+        }
+        q = q->next;
+    }
+    swap(p->people,pBegin->people);
+    return p;
+}
+
+void PeopleBook::quitSort(PeopleBook::Node *pBegin, PeopleBook::Node *pEnd) {
+    if (pBegin != pEnd)
+    {
+        Node * pivot_position = partition(pBegin,pEnd);
+        quitSort(pBegin,pivot_position);
+        quitSort(pivot_position->next,pEnd);
+    }
+}
+
+bool PeopleBook::sort() {
+    if (peoples < 2)
+    {
+        cout << "Can't sort because people less than two.\n";
+        return false;
+    }
+    else
+    {
+        quitSort(head,tail);
+        return true;
+    }
 }
