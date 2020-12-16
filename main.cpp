fa8ad7c57eec9ee847b85dd5f2c5bfb756b467fe
Hugo-spyder <53307317+Hugo-spyder@users.noreply.github.com>
2020-12-17 07:38:48 +0800

    Update main.cpp
    更改操作界面

--- main.cpp
+++ main.cpp
@@ -1,229 +1,206 @@
 #include "User.h"
 #include "people.h"
+#include <windows.h>
+#include <conio.h>
+#define red 12
+#define white 7
 
-void showMenu(int);
-
-void queryContact(PeopleBook &);
-
-void addContact(PeopleBook &);
-
-void modifyContact(PeopleBook &);
-
-void deleteContact(PeopleBook &);
-
-void displayContact(PeopleBook &);
-
-void fuzzyQuery(PeopleBook &);
+void color(int color)
+{
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),color);
+}
+void gotoxy(int x, int y)
+{
+    COORD c;
+    c.X = x;
+    c.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),c);
+}
 
 int main() {
     // 首先用户登录
-    int Select;
     User user;        // 创建用户对象
 
-    showMenu(1);
-    while (true) {
-        cout << "Choose(1, 2 or 3): \n";
-        while (!(cin >> Select)) {
-            cin.clear();
-            while (cin.get() != '\n')
-                continue;
-            cout << "Please enter a number: ";
+    int Set[3] = {red,white,white};
+    int counter = 1;
+    char key;
+
+    system("cls");
+    for(int i;;)
+    {
+        gotoxy(10,5);
+        color(Set[0]);
+        cout << "1. Login ";
+
+        gotoxy(10,6);
+        color(Set[1]);
+        cout << "2. Register ";
+
+        gotoxy(10,7);
+        color(Set[2]);
+        cout << "3. Quit ";
+
+        key = _getch();
+
+        if(key == 72 && (counter >= 2 && counter <= 3))    // 72 up arrow
+        {
+            counter--;
         }
-        cin.get();
-        if (Select == 3)
-            return 0;
-        cin >> user;       // 输入用户的信息：用户名 密码
-        if (Select == 1) {
-            if (user.signUp())
-                break;      // 注册成功，跳出循环
-            else
-                continue;   // 注册失败，跳出此次循环，继续登录或注册
-        } else if (Select == 2) {
-            if (user.signIn() == 1)
-                break;
-            else
-                continue;
+
+        if(key == 80 && (counter <= 2 && counter >= 1))   // 80 down arrow
+        {
+            counter++;
         }
+
+        if(key == '\r')
+        {
+            if(counter == 1)
+            {
+                gotoxy(0,8);
+                cin >> user;
+                if(user.signIn() == 1)
+                    break;
+                else
+                    continue;
+            }
+            if(counter == 2)
+            {
+                gotoxy(0,8);
+                cin >> user;
+                if (user.signUp())
+                    break;      // 注册成功，跳出循环
+                else
+                    continue;   // 注册失败，跳出此次循环，继续登录或注册
+            }
+            if(counter == 3)
+            {
+                system("cls");
+                color(white);
+                return 0;
+            }
+        }
+        // default color
+        Set[0] = white;
+        Set[1] = white;
+        Set[2] = white;
+        if (counter == 1)
+            Set[0] = red;
+        if (counter == 2)
+            Set[1] = red;
+        if (counter == 3)
+            Set[2] = red;
     }
+    color(white);
 
     // 联系人维护功能
     PeopleBook peopleBook;
-    peopleBook.loadData();      // 加载数据
+    peopleBook.loadData(user.getDataFile());      // 加载数据
+    int opt[8] = {red,white,white,white,white,white,white,white};
+    counter = 1;
 
-    while (true) {
-        cout << endl << user << "'s address book:\n";
-        showMenu(2);
-        cout << "People total: " << peopleBook.peopleCount() << endl;
-        cout << "Choose(from 1 to 6): \n";
-        while (!(cin >> Select)) {
-            cin.clear();
-            while (cin.get() != '\n')
-                continue;
-            cout << "Please enter a number: \n";
-        }
-        cin.get();
-        switch (Select) {
-            case 1:
-                queryContact(peopleBook);
-                break;
-            case 2:
-                addContact(peopleBook);
-                break;
-            case 3:
-                modifyContact(peopleBook);
-                break;
-            case 4:
-                deleteContact(peopleBook);
-                break;
-            case 5:
-                displayContact(peopleBook);
-                break;
-            case 6:
-                peopleBook.sort();
-                break;
-            case 7:
-                fuzzyQuery(peopleBook);
-                break;
-            default:
-                break;
-        }
-        if (Select == 8)
-            break;
-    }
-
-    peopleBook.outData();  // 将数据导入文件
-    return 0;
-}
-
-void showMenu(int choose) {
-    switch (choose) {
-        case 1:
-            cout << "\t\t^**********************^\n";
-            cout << "\t\t|     1. Sign up       |\n";
-            cout << "\t\t|     2. Sign in       |\n";
-            cout << "\t\t|     3. Quit          |\n";
-            cout << "\t\t^**********************^\n";
-            break;
-        case 2:
-            cout << "\t\t^*************************^\n";
-            cout << "\t\t|    1. Query contact     |\n";
-            cout << "\t\t|    2. Add contact       |\n";
-            cout << "\t\t|    3. Modify contact    |\n";
-            cout << "\t\t|    4. Delete contact    |\n";
-            cout << "\t\t|    5. Display contacts  |\n";
-            cout << "\t\t|    6. Sort              |\n";
-            cout << "\t\t|    7. Fuzzy Query       |\n";
-            cout << "\t\t|    8. Quit              |\n";
-            cout << "\t\t^*************************^\n";
-            break;
-        case 3:
-            cout << "\t\t^*************************^\n";
-            cout << "\t\t|    1. Name              |\n";
-            cout << "\t\t|    2. Telephone         |\n";
-            cout << "\t\t|    3. Address           |\n";
-            cout << "\t\t|    4. Mail              |\n";
-            cout << "\t\t|    5. Unit              |\n";
-            cout << "\t\t|    6. Comment           |\n";
-            cout << "\t\t|    7. Quit              |\n";
-            cout << "\t\t^*************************^\n";
-        default:
-            break;
-    }
-}
-
-void queryContact(PeopleBook &pB) {
-    string name;
-    cout << "Please enter the name of people who you are finding :\n";
-    getline(cin, name);
-    PeopleBook::Node *pN = pB.findpeople(name);
-    if (pN != nullptr)
-        cout << pN->people.getName() << "'s information:\n" << pN->people << endl;
-}
-
-void addContact(PeopleBook &pB) {
-    People contact;
-    cin >> contact;
-    if (pB.findpeople(contact.getName()) != nullptr)
+    system("cls");
+    for(int i;;)
     {
-        cout << "There is a people who has a same name in address book.\n";
-        return;
-    }
-    pB.addpeople(contact);
-}
+        gotoxy(10,5);
+        color(opt[0]);
+        cout << "1. Query contact ";
 
-void deleteContact(PeopleBook &pB) {
-    cout << "Please enter the name of the people who you want to delete :\n";
-    string name;
-    getline(cin, name);
-    pB.delpeople(name);
-}
+        gotoxy(10,6);
+        color(opt[1]);
+        cout << "2. Add contact ";
 
-void displayContact(PeopleBook &pB) {
-    cout << "How many do you want display?(enter a positive integer less than total people): \n";
-    int nums;
-    while (!(cin >> nums)) {
-        cin.clear();
-        while (cin.get() != '\n')
-            continue;
-        cout << "Please enter a number: ";
-    }
-    pB.showPeopleBk(nums);
-}
+        gotoxy(10,7);
+        color(opt[2]);
+        cout << "3. Modify contact ";
 
-void modifyContact(PeopleBook &pB) {
-    string name;
-    cout << "Please enter the name of people who you want modify: \n";
-    getline(cin, name);
-    PeopleBook::Node *pN = pB.findpeople(name);
-    if (pN == nullptr) {
-        cout << "Can't find the people whose name is " << name << endl;
-    } else {
-        cout << endl << pN->people << endl;
-        showMenu(3);
-        cout << "What information do you want modify?\n";
-        cout << "Choose(from 1 to 7): \n";
-        int Choice;
-        while (!(cin >> Choice)) {
-            cin.clear();
-            while (cin.get() != '\n')
-                continue;
-            cout << "Please enter a number: \n";
+        gotoxy(10,8);
+        color(opt[3]);
+        cout << "4. Delete contact ";
+
+        gotoxy(10,9);
+        color(opt[4]);
+        cout << "5. Display contacts ";
+
+        gotoxy(10,10);
+        color(opt[5]);
+        cout << "6. Sort ";
+
+        gotoxy(10,11);
+        color(opt[6]);
+        cout << "7. Fuzzy Query ";
+
+        gotoxy(10,12);
+        color(opt[7]);
+        cout << "8. Quit ";
+
+        color(white);
+        cout << endl << peopleBook.peopleCount() << " totally people have.";
+
+        key = _getch();
+
+        if(key == 72 && (counter >= 2 && counter <= 8))    // 72 up arrow
+        {
+            counter--;
         }
-        cin.get();
-        if (Choice == 7)
-            return;
-        string modifyInfo;
-        cout << "Please enter your modify:\n";
-        getline(cin, modifyInfo);
-        switch (Choice) {
-            case 1:
-                pN->people.setName(modifyInfo);
+
+        if(key == 80 && (counter <= 7 && counter >= 1))   // 80 down arrow
+        {
+            counter++;
+        }
+
+        if(key == '\r')
+        {
+            system("cls");
+            switch (counter) {
+                case 1:
+                    peopleBook.queryContact();
+                    break;
+                case 2:
+                    peopleBook.addContact();
+                    break;
+                case 3:
+                    peopleBook.modifyContact();
+                    break;
+                case 4:
+                    peopleBook.deletContact();
+                    break;
+                case 5:
+                    peopleBook.displayContact();
+                    break;
+                case 6:
+                    peopleBook.sort();
+                    break;
+                case 7:
+                    peopleBook.fuzzyQuery();
+                    break;
+                default: break;
+            }
+            if(counter == 8)
+            {
+                system("cls");
                 break;
-            case 2:
-                pN->people.setTele(modifyInfo);
-                break;
-            case 3:
-                pN->people.setAddr(modifyInfo);
-                break;
-            case 4:
-                pN->people.setMail(modifyInfo);
-                break;
-            case 5:
-                pN->people.setUnit(modifyInfo);
-                break;
-            case 6:
-                pN->people.setComm(modifyInfo);
-                break;
-            default:
-                break;
+            }
+            system("pause");
+            system("cls");
+        }
+        // default color
+        for(int & j : opt)
+            j = white;
+
+        switch (counter)
+        {
+            case 1: opt[0] = red; break;
+            case 2: opt[1] = red; break;
+            case 3: opt[2] = red; break;
+            case 4: opt[3] = red; break;
+            case 5: opt[4] = red; break;
+            case 6: opt[5] = red; break;
+            case 7: opt[6] = red; break;
+            case 8: opt[7] = red; break;
+            default: break;
         }
     }
-}
-
-void fuzzyQuery(PeopleBook & pB)
-{
-    cout << "Name:\n";
-    string name;
-    getline(cin,name);
-    pB.fuzzyQuery(name);
+    peopleBook.outData(user.getDataFile());
+    color(white);
+    return 0;
 }
